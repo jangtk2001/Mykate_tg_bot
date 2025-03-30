ecb03701cc7ca73ef61352d9d607232e6107717c
jangtk2001 <jangtk2001@gmail.com>
2025-03-30 20:52:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-print("Hello from Kate!")
 
+import os
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telegram.Bot(token=TOKEN)
 
+def handle_message(update, context):
+    user_text = update.message.text
+    response = f"Kate 說：你剛剛說了『{user_text}』對吧？我記下來囉。"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
+    updater.start_polling()
+    updater.idle()
 
-
-
-
-
-
-
-
-
-
-
-
-
-add main.py
+if __name__ == '__main__':
+    main()
